6fa599d631822a83b08877385fd47235774e41a4
Brittany Garrett <brittanymgarrett@gmail.com>
2022-02-07 21:19:11 -0600

    Initial all user list

--- src/components/auth/AuthProvider.js
+++ src/components/auth/AuthProvider.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react"
-
 export const ProfileContext = React.createContext()
 
 export const ProfileProvider = (props) => {
     const [profile, setProfile] = useState({events:[]})
+    const [users, setUsers] = useState([])
+
 
     const getProfile = () => {
         return fetch("http://localhost:8000/myprofile", {
@@ -16,9 +17,66 @@ export const ProfileProvider = (props) => {
             .then(setProfile)
     }
 
+    const getUsers = () => {
+        return fetch("http://localhost:8088/rareusers", {
+            method: "GET",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
+            }
+        })
+            .then(res => res.json())
+            .then(setUsers)
+    }
+
+
+    const getUserById = (user_id) => {
+		return fetch(`http://localhost:8088/rareusers/${user_id}`, {
+            method: "GET",
+            headers: {
+              "Authorization": `Token ${localStorage.getItem("rare_user_id")}`,
+            }
+        })
+			.then(res => res.json())
+    }
+
+    const addUsers = (userObj) => {
+        return fetch("http://localhost:8088/rareusers", {
+            method: "POST",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userObj)
+        })
+        .then(getUsers)
+    }
+
+    const updateUser = (user) => {
+        return fetch((`http://localhost:8088//items/${user.id}`), {
+            method: "PUT",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(getUsers)
+    }
+
+    const deleteUser = (userId) => {
+        return fetch((`http://localhost:8088/posts/${userId}`), {
+            method: "DELETE",
+            headers: {
+              "Authorization": `Token ${localStorage.getItem("rare_user_id")}`,
+              "Content-Type": "application/json"  
+            },
+        })
+            .then(getUsers)
+    }
+
     return (
         <ProfileContext.Provider value={{
-            profile, getProfile
+            profile, getProfile, users, setUsers, getUsers, addUsers, getUserById, updateUser, deleteUser
         }}>
             {props.children}
         </ProfileContext.Provider>
