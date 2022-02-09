f53fdcd67aed9ff338619b23b9ac3455a7b58442
Brittany Garrett <brittanymgarrett@gmail.com>
2022-02-08 22:01:47 -0600

    Updated some CSS

--- src/components/auth/Profile.js
+++ src/components/auth/Profile.js
@@ -1,30 +1,68 @@
-import React, { useEffect, useContext } from "react"
-import { ProfileContext } from "./AuthProvider.js"
-import "./Profile.css"
-
+import React, { useEffect, useContext } from "react";
+import { ProfileContext } from "./AuthProvider.js";
+import "./Profile.css";
 
 export const Profile = (props) => {
-    const { profile, getProfile } = useContext(ProfileContext)
+  const { profile, getProfile } = useContext(ProfileContext);
 
-    useEffect(() => {
-        getProfile()
-    }, [])
+  useEffect(() => {
+    getProfile();
+  }, []);
 
-    return (
-        <article className="profile">
-            <header>
-                <h1>Your Profile</h1>
-            </header>
-            <section className="profile__info">
-                <header className="profile__header">
-                    <h3>Your Info</h3>
-                </header>
-                <div className="profile__name">
-                    Welcome: {profile.rareuser && profile.rareuser.user.first_name} {profile.rareuser && profile.rareuser.user.last_name}
-                </div>
-                <div className="profile__username">Username: {profile.rareuser && profile.rareuser.user.username}</div>
-                <div className="profile__bio">About you: {profile.rareuser && profile.rareuser.bio}</div>
-            </section>
-        </article>
-    )
+  /*
+.userListWrapper {
+    margin: -2em 6em;
+    border: 1px solid gray;
+    padding: 1em;
 }
+
+.allUserList {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-left: 20px;
+}
+*/
+
+  return (
+    <>
+      <article
+        className="profile"
+        style={{
+          margin: " -2em 6em",
+          border: " 1px solid lightgray",
+          padding: "1em",
+        }}
+      >
+          <header>
+            <h1>Your Profile</h1>
+          </header>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            marginLeft: "20px",
+          }}
+        >
+        <section className="profile__info">
+          <header className="profile__header">
+            <h3>Your Info</h3>
+          </header>
+          <div className="profile__name">
+            Welcome: {profile.rareuser && profile.rareuser.user.first_name}{" "}
+            {profile.rareuser && profile.rareuser.user.last_name}
+          </div>
+          <div className="profile__username">
+            Username: {profile.rareuser && profile.rareuser.user.username}
+          </div>
+          <div className="profile__bio">
+            About you: {profile.rareuser && profile.rareuser.bio}
+          </div>
+        </section>
+
+        </div>
+      </article>
+    </>
+  );
+};
